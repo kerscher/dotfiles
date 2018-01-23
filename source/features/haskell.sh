cecb0376fe60223e6e962c9288a71baa20af40bf
Yghor Kerscher <kerscher@outlook.com>
2018-01-23 19:25:45 +0100

    Add `stack` Bash auto-completions

--- source/features/haskell.sh
+++ source/features/haskell.sh
@@ -5,6 +5,7 @@ setup_haskell() {
         if [ ! -f "${HOME}/.ghci" ]; then
             ln -s "${DOTFILES}/config/ghci" "${HOME}/.ghci"
         fi
+        eval "$(stack --bash-completion-script stack)"
         DOTFILES_FEATURES="haskell ${DOTFILES_FEATURES}"
     else
         log_error "Haskell toolset error: reinstall stack and try again."
