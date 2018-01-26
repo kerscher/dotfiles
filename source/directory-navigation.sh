1ff861e655bcf403e935e33321228a0fb94c5277
Yghor Kerscher <kerscher@outlook.com>
2018-01-26 22:32:28 +0100

    directory-navigation: protect against pushd failure

--- source/directory-navigation.sh
+++ source/directory-navigation.sh
@@ -2,7 +2,7 @@
 
 change_directory() {
     if hash cdargs 2>/dev/null; then
-        cdargs "$1" && pushd "$(cat "$HOME/.cdargsresult")"
+        cdargs "$1" && pushd "$(cat "$HOME/.cdargsresult")" || exit
     else
         log_error "Error: cdargs is not on PATH. Ensure it's installed and try again."
     fi
