c43d4e0e987716c145afe48ad65580f190c9f4ef
Yghor Kerscher <kerscher@acm.org>
2018-06-16 18:03:15 +0200

    Add Fedora support
    Should probably work on Red Hat Enterprise Linux or CentOS also, but untested

--- source/bootstrap.sh
+++ source/bootstrap.sh
@@ -1,20 +1,45 @@
 #!/bin/bash
 
-sudo add-apt-repository ppa:ubuntu-elisp/ppa
-sudo apt update
-sudo apt dist-upgrade
-sudo apt install \
-     build-essential cmake ninja-build \
-     ca-certificates \
-     curl git \
-     docker.io \
-     emacs-snapshot \
-     jq \
-     keychain monkeysphere \
-     libssl-dev libsqlite3-dev xorg-dev libbz2-dev libreadline-dev lzma \
-     pass \
-     tree \
-     zbar-tools qrencode oathtool
+if [[ -f /etc/redhat-release ]]; then
+    sudo dnf install install \
+         cmake \
+         ca-certificates \
+         curl \
+         git \
+         docker-latest \
+         jq \
+         keychain \
+         monkeysphere \
+         openssl-devel \
+         bzip2-devel \
+         readline-devel \
+         lzma-devel \
+         sqlite-devel \
+         qrencode \
+         zbar \
+         oathtool \
+         pass \
+         pass-otp \
+         qtpass
+fi
+
+if [[ -f /etc/lsb_release ]]; then   
+    sudo add-apt-repository ppa:ubuntu-elisp/ppa
+    sudo apt update
+    sudo apt dist-upgrade
+    sudo apt install \
+         build-essential cmake ninja-build \
+         ca-certificates \
+         curl git \
+         docker.io \
+         emacs-snapshot \
+         jq \
+         keychain monkeysphere \
+         libssl-dev libsqlite3-dev xorg-dev libbz2-dev libreadline-dev lzma \
+         pass \
+         tree \
+         zbar-tools qrencode oathtool
+fi
 
 curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
 git clone https://github.com/kamatama41/tfenv.git ~/.tfenv
@@ -27,3 +52,28 @@ sudo chown -R "${USER}:${USER}" /usr/local/plan9
 pushd /usr/local/plan9
 ./INSTALL
 popd
+
+install_go_tools() {
+    for t in alecthomas/gometalinter liudng/dogo mitchellh/gox svent/sift; do
+        go get -u "github.com/${t}"
+    done
+
+    gometalinter --install
+}
+
+install_rust_tools() {
+    cargo install \
+          clog-cli \
+          drill \
+          funzzy \
+          just \
+          ripgrep \
+          xsv
+}
+
+install_haskell_tools() {
+    stack install \
+          brittany \
+          hindent \
+          pandoc
+}
