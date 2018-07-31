78a46642f68d7ba286cf5b0cf48f33cfe6153acc
Yghor Kerscher <yghor.kerscher@sumdog.com>
2018-07-31 16:06:35 +0100

    Provide `tf` as function for `aws-env -p default terraform $@`

--- source/features/terraform.sh
+++ source/features/terraform.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-tf_feedback() {
-    while sleep 1
-    do
-        find . -maxdepth 1 -iname '*.tf' \
-            | entr -c -d sh -c \
-                   'terraform init && terraform validate'
-    done
-}
-
 setup_terraform() {
     TFENV_ROOT="${HOME}/.tfenv"
     TFENV_PATH="${TFENV_ROOT}/bin"
     if [ -d "${TFENV_ROOT}" ]; then
         export PATH=${TFENV_PATH}:${PATH}
+
+        tf() { aws-env -p default terraform "$@"; };
+
+        tf_feedback() {
+            while sleep 1
+            do
+                find . -maxdepth 1 -iname '*.tf' \
+                    | entr -c -d sh -c \
+                           'tf init && tf validate'
+            done
+        }
     else
         log_error "Terraform toolset error: \"${TFENV_ROOT}\" does not exist. Install from https://github.com/kamatama41/tfenv."
     fi
