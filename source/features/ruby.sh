b0f5fe6ac808881f88dc64257a16b3973da4f2cd
Yghor Kerscher <yghor.kerscher@sumdog.com>
2018-08-15 09:37:06 +0100

    feat(ruby): execute `sd` from `sumdog_tools`

--- source/features/ruby.sh
+++ source/features/ruby.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+sumdog_tools() {
+    if [ ! -z "${SUMDOG_TOOLS_PATH}" ]; then
+        pushd "${SUMDOG_TOOLS_PATH}" > /dev/null || return 
+        bundle exec sd "$@"
+        popd > /dev/null || return
+    else
+        log_error "SUMDOG_TOOLS_PATH not set. Export it with an absolute path."
+    fi
+}
+
 setup_ruby() {
     RBENV_PATH="${HOME}/.rbenv/bin"
     if [ -d "${RBENV_PATH}" ]; then
