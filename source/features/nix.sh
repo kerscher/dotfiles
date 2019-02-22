66f316cf1e1bfa54732b14acf617f0fdc4414ba3
Yghor Kerscher <yghor.kerscher@sumdog.com>
2019-02-22 17:45:40 +0000

    fix(nix): correct check for nix startup and enable it

--- source/features/nix.sh
+++ source/features/nix.sh
@@ -2,10 +2,10 @@
 
 setup_nix() {
     NIX_PROFILE_SH="${HOME}/.nix-profile/etc/profile.d/nix.sh"
-    if [ -e "${NIX_PROFILE_SH}" ]; then
+    if [ -f "${NIX_PROFILE_SH}" ]; then
         # shellcheck source=/dev/null
         . "${NIX_PROFILE_SH}";
     fi
     unset NIX_PROFILE_SH
 }
-
+setup_nix
