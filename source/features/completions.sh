1dde811349bfa603bc2487943c31e0a83d163378
Yghor Kerscher <kerscher@acm.org>
2019-10-27 13:09:00 +0000

    feat(completions): enable system completions
    Signed-off-by: Yghor Kerscher <kerscher@acm.org>

--- source/features/completions.sh
+++ source/features/completions.sh
@@ -9,3 +9,31 @@ if ! shopt -oq posix; then
         . /etc/bash_completion
     fi
 fi
+
+if type brew &> /dev/null
+then
+    HOMEBREW_PREFIX="$(brew --prefix)"
+    HOMEBREW_COMPLETION_COMPAT_DIR="${HOMEBREW_PREFIX}/etc/bash_completion.d"
+    [[ -d "${HOMEBREW_COMPLETION_COMPAT_DIR}" ]] && export BASH_COMPLETION_COMPAT_DIR="${HOMEBREW_COMPLETION_COMPAT_DIR}"
+    if [[ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]
+    then
+        # shellcheck source=/dev/null
+        source "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
+    else
+        for c in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*
+        do
+            # shellcheck source=/dev/null
+            [ -r "${c}" ] && source "${c}"
+        done
+    fi
+    # shellcheck source=/dev/null
+    BASH_SYSTEM_COMPLETIONS='/usr/local/etc/bash_completion.d'
+    if [ -d "${BASH_SYSTEM_COMPLETIONS}" ]
+    then
+        for c in "${BASH_SYSTEM_COMPLETIONS}/"*
+        do
+            # shellcheck source=/dev/null
+            [ -r "${c}" ] && source "${c}"
+        done
+    fi
+fi
