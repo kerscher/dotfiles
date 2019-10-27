a428d0594a3a6588f1898b01194b759f9125f7c9
Yghor Kerscher <kerscher@acm.org>
2019-10-27 13:07:15 +0000

    feat(macOS): bootstrap with `brew`
    Signed-off-by: Yghor Kerscher <kerscher@acm.org>

--- source/bootstrap.sh
+++ source/bootstrap.sh
@@ -6,52 +6,85 @@ set -o pipefail
 
 echo 'dotfiles: bootstrapping system packages. You will be asked for super-user permissions.'
 
+if (type sw_vers &> /dev/null) || (type brew &> /dev/null)
+then
+    declare -a macOS_brew_packages=(
+        'bash'
+        'bash-completions@2'
+        'brew-cask-completion'
+        'cargo-completion'
+        'coreutils'
+        'docker'
+        'docker-compose'
+        'docker-machine'
+        'docker-machine-driver-xhyve'
+        'moreutils'
+        'packer-completion'
+        'pass'
+        'pass-otp'
+        'pip-completion'
+        'yq'
+        'xhyve'
+    )
+    declare -a macOS_brew_cask_packages=(
+        'firefox'
+        'google-chrome'
+        'keepassxc'
+        'minikube'
+        'tunnelblick'
+        'vagrant'
+        'zoomus'
+    )
+    brew install "${macOS_brew_packages[@]}"
+    brew cask install "${macOS_brew_cask_packages[@]}"
+fi
+
 if [[ -f /etc/redhat-release ]]; then
     declare -a fedora_packages=(
-        "autoconf"
-        "automake"
-        "bzip2"
-        "bzip2-devel"
-        "ca-certificates"
-        "cmake"
-        "curl"
-        "dnf-plugins-core"
-        "emacs"
-        "fontconfig-devel"
-        "freetype-devel"
-        "gcc"
-        "git"
-        "jq"
-        "keychain"
-        "libXext-devel"
-        "libXt-devel"
-        "libffi-devel"
-        "libicu-devel"
-        "libtool"
-        "libxslt-devel"
-        "libyaml-devel"
-        "lzma-devel"
-        "make"
-        "monkeysphere"
-        "ncurses-compat-libs"
-        "ncurses-devel"
-        "oathtool"
-        "openssl-devel"
-        "pass"
-        "pass-otp"
-        "qrencode"
-        "qtpass"
-        "readline-devel"
-        "sqlite"
-        "sqlite-devel"
-        "tilda"
-        "tk-devel"
-        "unixODBC-devel"
-        "unzip"
-        "xorg-x11-server-devel"
-        "xz"
-        "zbar"
-        "zlib-devel"
+        'autoconf'
+        'automake'
+        'bzip2'
+        'bzip2-devel'
+        'ca-certificates'
+        'cmake'
+        'curl'
+        'dnf-plugins-core'
+        'emacs'
+        'fontconfig-devel'
+        'freetype-devel'
+        'gcc'
+        'git'
+        'jq'
+        'keychain'
+        'libXext-devel'
+        'libXt-devel'
+        'libffi-devel'
+        'libicu-devel'
+        'libtool'
+        'libxslt-devel'
+        'libyaml-devel'
+        'lzma-devel'
+        'make'
+        'monkeysphere'
+        'ncurses-compat-libs'
+        'ncurses-devel'
+        'oathtool'
+        'openssl-devel'
+        'pass'
+        'pass-otp'
+        'qrencode'
+        'qtpass'
+        'readline-devel'
+        'sqlite'
+        'sqlite-devel'
+        'tilda'
+        'tk-devel'
+        'unixODBC-devel'
+        'unzip'
+        'xorg-x11-server-devel'
+        'xz'
+        'zbar'
+        'zlib-devel'
     )
     sudo dnf install "${fedora_packages[@]}"
     sudo dnf config-manager \
