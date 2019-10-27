bc2b5a40e341d6b6c84c007ae154c679b52fc1ea
Yghor Kerscher <kerscher@acm.org>
2019-10-27 13:09:36 +0000

    feat(go): enable ASDF for version management
    Signed-off-by: Yghor Kerscher <kerscher@acm.org>

--- source/features/go.sh
+++ source/features/go.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 : "${HOME?}"
+: "${ASDF_HOME?}"
 
 if ! test "$(type -t asdf_bootstrap = 'function')"
 then
@@ -12,20 +13,20 @@ GOLANG_DOTFILES_VERSION='1.13'
 
 setup_go() {
     asdf_bootstrap 'golang' "${GOLANG_DOTFILES_VERSION}"
-    
-    GOPATH="${HOME}/go"
-    if [ ! -d "${GOPATH}" ]; then
-        mkdir "${GOPATH}"
-        export GOPATH="${GOPATH}"
-    fi
-    export PATH=${GOPATH}/bin:${PATH}
+
+    GO_ASDF_PATH="${ASDF_HOME}/installs/golang/${GOLANG_DOTFILES_VERSION}"
+    GOPATH="${GO_ASDF_PATH}/packages"
+    GOROOT="${GO_ASDF_PATH}/go"
+    GO111MODULE='on'
+    export GOPATH GOROOT GO111MODULE
+
     DOTFILES_FEATURES="go ${DOTFILES_FEATURES}"
 }
 
 setup_go
 
 install_go_tools() {
-    declare -a go_packages=(
+    declare -a go_github_packages=(
         "astaxie/bat"
         "golangci/golangci-lint/cmd/golangci-lint"
         "jessfraz/dockfmt"
@@ -36,7 +37,7 @@ install_go_tools() {
         "sourcegraph/go-langserver"
         "svent/sift"
     )
-    for t in "${go_packages[@]}"; do
+    for t in "${go_github_packages[@]}"; do
         go get -u "github.com/${t}"
     done
 }
