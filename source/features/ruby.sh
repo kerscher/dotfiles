7aa60683451b683544b49d8016ff61616e1d605a
Yghor Kerscher <kerscher@acm.org>
2019-10-31 21:45:24 +0000

    feat(ruby): set PATH to include Ruby binaries from ASDF
    Signed-off-by: Yghor Kerscher <kerscher@acm.org>

--- source/features/ruby.sh
+++ source/features/ruby.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 : "${HOME?}"
+: "${ASDF_HOME?}"
 
 if ! test "$(type -t asdf_bootstrap = 'function')"
 then
@@ -12,6 +13,13 @@ RUBY_DOTFILES_VERSION='2.6.3'
 
 setup_ruby() {
     asdf_bootstrap 'ruby' "${RUBY_DOTFILES_VERSION}"
+    RUBY_BIN_PATH="${ASDF_HOME}/installs/ruby/${RUBY_DOTFILES_VERSION}/bin"
+    if [ -d "${RUBY_BIN_PATH}" ]
+    then
+        export RUBY_BIN_PATH
+        export PATH="${RUBY_BIN_PATH}:${PATH}"
+    else log_error "Could not add Ruby ${RUBY_DOTFILES_VERSION} to path."
+    fi
     DOTFILES_FEATURES="ruby ${DOTFILES_FEATURES}"
 }
 
