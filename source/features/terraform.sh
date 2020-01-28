7225a353a1f17c40439d168582d05e214c039fc1
Yghor Kerscher <kerscher@acm.org>
2020-01-28 11:08:00 +0000

    feat(terraform): update from `v0.12.16` to `v0.12.20`
    Signed-off-by: Yghor Kerscher <kerscher@acm.org>

--- source/features/terraform.sh
+++ source/features/terraform.sh
@@ -6,7 +6,7 @@ then
     return
 fi
 
-TERRAFORM_DOTFILES_VERSION='0.12.16'
+TERRAFORM_DOTFILES_VERSION='0.12.20'
 
 setup_terraform() {
     asdf_bootstrap 'terraform' "${TERRAFORM_DOTFILES_VERSION}"
